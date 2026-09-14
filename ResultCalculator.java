db636b1651772929c38f55861b8747bf181412a9
pranshu1392-bit <pranshu1392@email.com>
2026-09-14 22:19:56 +0530

    Merge GitHub changes

---
+++ ResultCalculator.java
@@ -0,0 +1,41 @@
+public class ResultCalculator {
+
+    static int total(int[] marks) {
+        int sum = 0;
+
+        for (int mark : marks)
+            sum += mark;
+
+        return sum;
+    }
+
+    static double percentage(int[] marks) {
+        return total(marks) / 5.0;
+    }
+
+    static String grade(double percentage) {
+
+        if (percentage >= 90)
+            return "A+";
+        else if (percentage >= 80)
+            return "A";
+        else if (percentage >= 70)
+            return "B";
+        else if (percentage >= 60)
+            return "C";
+        else if (percentage >= 50)
+            return "D";
+        else
+            return "F";
+    }
+
+    static String status(int[] marks) {
+
+        for (int mark : marks) {
+            if (mark < 40)
+                return "FAIL";
+        }
+
+        return "PASS";
+    }
+}
