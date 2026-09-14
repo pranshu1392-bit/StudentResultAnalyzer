a70561770ba7efab1ce2bf2dc57194bcf66c1531
Pranshu Singh <pranshu1392@gmail.com>
2026-09-14 22:00:39 +0530

    Add files via upload

---
+++ Student.java
@@ -0,0 +1,12 @@
+public class Student {
+
+    int id;
+    String name;
+    int[] marks;
+
+    Student(int id, String name, int[] marks) {
+        this.id = id;
+        this.name = name;
+        this.marks = marks;
+    }
+}
